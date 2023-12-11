0d61f95b24401c5e4b3ee51fc8fc98f5615be0c4
yeongsbook <yeongsbook@gmail.com>
2023-12-11 22:02:31 +0900

    2. 캔버스 엘리먼트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+const DEFAULT_CANVAS_WIDTH = 600;
+const DEFAULT_CANVAS_HEIGHT = 300;
 
 function App() {
-  return <div></div>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+    const dpr = window.devicePixelRatio;
+    canvas.style.width = DEFAULT_CANVAS_WIDTH + "px";
+    canvas.style.height = DEFAULT_CANVAS_HEIGHT + "px";
+    canvas.width = DEFAULT_CANVAS_WIDTH * dpr;
+    canvas.height = DEFAULT_CANVAS_HEIGHT * dpr;
+    ctx.scale(dpr, dpr);
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, DEFAULT_CANVAS_WIDTH, DEFAULT_CANVAS_HEIGHT);
+  }, []);
+
+  return (
+    <div>
+      <canvas ref={canvasRef} />
+    </div>
+  );
 }
 
 export default App;
